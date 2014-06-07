ccc7c475483be67dd5d7c9528bb350d4a17d5cc5
William Wilkins <william.wilkins@gmail.com>
2014-06-06 22:03:01 -0400

    Programming Assignment 2 W. Wilkins
    Just updated comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions prepare a cache for an invertable matrix and either retrieve the inverted
+##matrix from the cache or computes the inverted matrix and caches it for later us.
 
-## Write a short comment describing this function
+## This function creates a special "matrix", which will allow the user to set/get the value
+## of the matrix to be inverted and to set/get the cached inverted matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function first checks to see if the inverted matrix has been cached, and if so returns
+## it. If the inverted matrix has not been cached, this function will compute the inverted
+## matrix, store it in the cache, and return it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
